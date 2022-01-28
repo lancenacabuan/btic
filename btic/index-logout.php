fa6f2bd7060f76d508ba8894af8d2dc24c0c095d
Lance Nacabuan <lancenacabuan@outlook.com>
2022-01-28 09:03:12 +0800

    Add files via upload

---
+++ btic/index-logout.php
@@ -0,0 +1,6 @@
+<?php //********************BTIC Invoicing & Payroll System v14.22.0127.1855********************//
+include('functions.php');
+session_unset();
+session_destroy();
+redirect_page();
+?>
