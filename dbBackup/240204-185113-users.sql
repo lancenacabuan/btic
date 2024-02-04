92283c26ba5e03c96b5c3c4ceffe4b8e47106951
Lance Nacabuan <lancenacabuan@outlook.com>
2024-02-04 19:01:07 +0800

    dbBackup

---
+++ dbBackup/240204-185113-users.sql
@@ -0,0 +1,5 @@
+1	btic_admin	Nacabuan	Lorenzo Martin	lance	nacabuan	WARNING: This user account is restricted!!! Continue???	yes
+2	btic_admin	Gumangan	Cristina	tina	04251984	When is my birthday???	04251984
+3	btic_payroll	Zafe	Ana	btic	btic	What is the company name???	btic
+4	btic_sales	Verano	Jennifer	btic_ar	frozenyogurt	What is the name of our main product???	frozenyogurt
+5	btic_payroll	Flores	Milagros	miles	miles	miles	miles
